eaf80847a0f6f5f1e7721f428fff23f44c80da2f
David Kong <dkong8s2017@gmail.com>
2021-08-04 20:18:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          PokeWiki
+        </p>
+      </header>
+      <Dashboard />
+    </div>
+  );
+}
+
+
+export default App;
